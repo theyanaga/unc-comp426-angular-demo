b745b9d4667a9d2f3c4e8489ae6952b5b25d7971
theyanaga <felipe@yanaga.com.br>
2024-04-15 22:37:18 -0400

    added event handling for passing cards

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { ControllerService } from '../controller.service';
 
 
 export class Card {
@@ -10,8 +11,6 @@ export class Card {
     this.rank = rank;
   }
 
-
-
 }
 
 @Component({
@@ -21,6 +20,8 @@ export class Card {
 })
 export class CardComponent {
   @Input('card') card: Card = new Card("", 1);
+
+  constructor(private controllerService: ControllerService) {}
 
   public getSuit() : string {
     return this.card.suit;
@@ -32,5 +33,9 @@ export class CardComponent {
 
   public getImageUrl() : string {
     return "https://deckofcardsapi.com/static/img/" + this.getRank() + this.getSuit() + ".png";
+  }
+
+  public handleSelectionForPass() : void {
+    this.controllerService.selectCardToPass(this.card);
   }
 }
--- src/app/controller.service.ts
+++ src/app/controller.service.ts
@@ -8,6 +8,7 @@ export class ControllerService {
   private state: string = "initial";
   private name: string = "";
   private cards : Card[] = [];
+  private cardsToPass : Card[] = [];
 
   constructor() {
     let suits = ["H", "S", "D", "C"]
@@ -16,6 +17,7 @@ export class ControllerService {
       return ranks.map(r => new Card(s, r))
     });
    }
+
 
   public getName() : string {
     return this.name;
@@ -43,6 +45,15 @@ export class ControllerService {
 
   public isPassing() : boolean {
     return this.state === "passing";
+  }
+
+  public selectCardToPass(card: Card) : void {
+    this.cardsToPass.push(card);
+    console.log(this.cardsToPass)
+    if (this.cardsToPass.length === 3) {
+      this.state = "playing";
+      this.cardsToPass = [];
+    }
   }
 
 }
